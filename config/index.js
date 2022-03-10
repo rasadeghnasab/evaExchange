40244c1d01444f087d1bf44a931e5124e7bb3784
Ramin <ramin.sadeghnasab@foodmarkethub.com>
2022-03-10 22:35:18 +0330

    - Add config functionality to the app

---
+++ config/index.js
@@ -0,0 +1,32 @@
+const loadConfigs = () => {
+    const normalizedPath = require("path").join(__dirname);
+    const files = require("fs").readdirSync(normalizedPath).filter((file) => file !== 'index.js');
+
+    let configs = {};
+
+    files.forEach((config) => {
+        config = config.replace('.js', '');
+        configs[config] = require(`./${config}`)
+    });
+
+    return configs;
+}
+
+const get = (path, defaultVal = '') => {
+    const parts = path.split('.');
+    let config = loadConfigs();
+
+    try {
+        for (const configKey of parts) {
+            config = config[configKey];
+        }
+    } catch {
+        return defaultVal;
+    }
+
+    return config;
+};
+
+module.exports = {
+    get
+};
