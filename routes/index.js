ef4e6e5c77c0f3dc420e3fd3e158200b2168ae54
Ramin <ramin.sadeghnasab@foodmarkethub.com>
2022-03-12 21:32:39 +0330

    - Update CORS functionality

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,20 @@ const appRoutes = new Router();
 const koaJson = require('koa-json');
 const cors = require('@koa/cors');
 
-const apiRoutes = require('./api');
+const configs = require('../config');
 const errorHandlerMiddleware = require("../app/http/middlewares/errorHandlingMiddleware");
 const routeNotFoundMiddleware = require("../app/http/middlewares/routeNotFoundMiddleware");
 
-appRoutes.use('/api/', apiRoutes.routes(), apiRoutes.allowedMethods());
+const apiRoutes = require('./api').prefix('/api');
+
+appRoutes.use(apiRoutes.routes(), apiRoutes.allowedMethods());
 
 console.log(appRoutes.stack.map(i => i.path));
 
 module.exports = (app) => {
     app.use(errorHandlerMiddleware);
     app.use(koaJson());
-    app.use(cors());
+    app.use(cors(configs.get('cors')));
     app.use(appRoutes.routes(), appRoutes.allowedMethods());
     app.use(routeNotFoundMiddleware);
 };
